f55192e20d34a00a1dd3b42a91e8235fa8bdce09
Trung DT <thanhtrung.it25@gmail.com>
2024-01-13 14:32:28 +0700

    Modal component admin

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,18 @@
-import { UserButton } from '@clerk/nextjs'
+'use client'
+
+import Modal from '@/components/ui/modal'
+import { useStoreModal } from '@/hooks/use-store-modal'
+import { useEffect } from 'react'
 
 export default function SetupPage() {
-  return (
-    <div className="p-4">
-      <UserButton />
-    </div>
-  )
+  const isOpen = useStoreModal((state) => state.isOpen)
+  const onOpen = useStoreModal((state) => state.onOpen)
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen()
+    }
+  }, [isOpen, onOpen])
+
+  return <div className="p-4">Root Page</div>
 }
