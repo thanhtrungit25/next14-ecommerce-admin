2090cd7ab23df889584a241c4be51a9dcc06d7ab
Trung DT <thanhtrung.it25@gmail.com>
2024-01-26 14:45:40 +0700

    Colors Entity Admin

---
+++ app/(dashboard)/[storeId]/(routes)/colors/components/client.tsx
@@ -0,0 +1,41 @@
+'use client'
+
+import { Plus } from 'lucide-react'
+import { useParams, useRouter } from 'next/navigation'
+
+import Heading from '@/components/ui/Heading'
+import { Button } from '@/components/ui/button'
+import { Separator } from '@/components/ui/separator'
+import { DataTable } from '@/components/ui/data-table'
+import { ApiList } from '@/components/ui/api-list'
+import { ColorColumn, columns } from './columns'
+
+interface ColorsClientProps {
+  colors: ColorColumn[]
+}
+
+const ColorsClient: React.FC<ColorsClientProps> = ({ colors }) => {
+  const router = useRouter()
+  const params = useParams()
+
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading
+          title={`Colors (${colors.length})`}
+          description="Manage colors for your store"
+        />
+        <Button onClick={() => router.push(`/${params.storeId}/colors/new`)}>
+          <Plus className="mr-2 w-4" />
+          Add New
+        </Button>
+      </div>
+      <Separator />
+      <DataTable columns={columns} data={colors} searchKey="name" />
+      <Heading title="API" description="API calls for colors" />
+      <Separator />
+      <ApiList entityName="colors" entityIdName="colorId" />
+    </>
+  )
+}
+export default ColorsClient
