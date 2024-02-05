196ccc85f361cfa01ab9b985a1a2dd92d2c59b3b
Trung DT <thanhtrung.it25@gmail.com>
2024-02-05 11:39:46 +0700

    Dashboard Page

---
+++ actions/get-sales-count.ts
@@ -0,0 +1,12 @@
+import prismadb from '@/prisma/prismadb'
+
+export const getSalesCount = async (storeId: string) => {
+  const salesCount = await prismadb.order.count({
+    where: {
+      storeId,
+      isPaid: true,
+    },
+  })
+
+  return salesCount
+}
---
+++ actions/get-stock-count.ts
@@ -0,0 +1,12 @@
+import prismadb from '@/prisma/prismadb'
+
+export const getStockCount = async (storeId: string) => {
+  const stockCount = await prismadb.product.count({
+    where: {
+      storeId,
+      isArchived: false,
+    },
+  })
+
+  return stockCount
+}
